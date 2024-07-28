aa550279e7f505a3a2a238a5dd52d0cf81a4d4d9
jakel <jakelogan35@gmail.com>
2024-07-28 14:16:33 +0100

    cmt 4

--- web_scraper.py
+++ web_scraper.py
@@ -37,28 +37,37 @@ def fetch_medal_data(url):
         print("Failed to retrieve or parse page")
         return None
 
+
 def parse_medals(soup):
-    #print(soup)
     rows = soup.find_all('tr', class_='ssrcss-dhlz6k-TableRowBody e1xoxfm60')
     medal_data = []
     for row in rows:
-        country = row.find('span', class_='ssrcss-pek3um-AbbreviatedCountryName ew4ldjd1').text.strip()
-        gold = row.find('td', class_='ssrcss-fvkmzs-StyledTableData ef9ipf1').text.strip()
-        silver = row.find('td', class_='ssrcss-fvkmzs-StyledTableData ef9ipf1').text.strip()
-        bronze = row.find('td', class_='ssrcss-fvkmzs-StyledTableData ef9ipf1').text.strip()
+        cells = row.find_all('td', class_='ssrcss-fvkmzs-StyledTableData ef9ipf1')
+
+        current_ranking = cells[0].find('div').text.strip() if cells[0].find('div') else 'N/A'
+        country_name_div = cells[1].find('span', class_='ssrcss-ymac56-CountryName ew4ldjd0')
+        country_name = country_name_div.text.strip() if country_name_div else 'N/A'
+
+        gold = cells[2].find('div').text.strip()
+        silver = cells[3].find('div').text.strip()
+        bronze = cells[4].find('div').text.strip()
+        overall = cells[5].find('div').text.strip()
 
         medal_data.append({
-            'country': country,
+            'ranking': current_ranking,
+            'country': country_name,
             'gold': gold,
             'silver': silver,
-            'bronze': bronze
+            'bronze': bronze,
+            'overall': overall
         })
     return medal_data
-
 
 if __name__ == '__main__':
     url = 'https://www.bbc.co.uk/sport/olympics/paris-2024/medals'
     html_soup = fetch_medal_data(url)
     if html_soup:
         results = parse_medals(html_soup)
-        print(results)
+        formatted_results = "\n".join(str(result) for result in results)
+        #print(results)
+        print(formatted_results)
